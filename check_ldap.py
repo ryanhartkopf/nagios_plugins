9bdaddd754e4dfbf3b120f96b5ab8ff98fabeb10
Ryan Hartkopf <rhartkopf@appliedtrust.com>
2014-06-04 21:35:44 -0600

    Resolve compatibilty errors, disable strict key checking

--- check_ldap.py
+++ check_ldap.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import ldap,argparse
+import ldap,argparse,sys
 
 parser = argparse.ArgumentParser(description='Performs a simple LDAP search with filter against a secured or unsecured LDAP server, and returns OK if any results are returned.', epilog="Usage example: check_ldap -H localhost -B 'dn=example,dn=com' -K 'cn' -V 'john*' -D 'cn=james,o=users,dn=example,dn=com' -P 'passw0rd' -p 8636 --secure")
 parser.add_argument('-H', dest='server', help="LDAP server on which the query will be run", required=True)
@@ -27,8 +27,6 @@ parser.add_argument('-p', dest='port', help="Optional port setting")
 parser.add_argument('--secure', dest='secure', help="Enable secured connection", action="store_true")
 args = parser.parse_args()
 
-print args.secure
-
 if args.secure == True:
   full_server = 'ldaps://' + args.server
 else:
@@ -37,6 +35,7 @@ if args.port:
   full_server += ':' + args.port
 
 l = ldap.initialize(full_server)
+ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
 
 if args.binddn and args.bindpw:
   l.simple_bind_s( args.binddn, args.bindpw )
@@ -50,7 +49,7 @@ results = l.search_s(args.basedn,ldap.SCOPE_SUBTREE,filter,ret)
 if results:
   print "OK: Search for object " + args.value + " was successful!"
   print results
-  exit()
+  sys.exit(0)
 else:
   print "CRITICAL: Search failed!"
-  exit(2)
+  sys.exit(2)
