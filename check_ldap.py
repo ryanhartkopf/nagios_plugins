3890d8e4a329e1d2df7f77f33be9423b2a8115e0
Ryan Hartkopf <rhartkopf@appliedtrust.com>
2014-11-14 13:45:00 -0700

    Require key and value pair for search

--- check_ldap.py
+++ check_ldap.py
@@ -16,11 +16,11 @@
 
 import ldap,argparse,sys
 
-parser = argparse.ArgumentParser(description='Performs a simple LDAP search with filter against a secured or unsecured LDAP server, and returns OK if any results are returned.', epilog="Usage example: check_ldap -H localhost -B 'dn=example,dn=com' -K 'cn' -V 'john*' -D 'cn=james,o=users,dn=example,dn=com' -P 'passw0rd' -p 8636 --secure")
+parser = argparse.ArgumentParser(description='Performs a simple LDAP search with filter against a secured or unsecured LDAP server, and returns OK if any results are returned.', epilog="Usage example: check_ldap -H localhost -B 'dn=example,dn=com' -K 'cn' -V 'john*' -U 'cn=james,o=users,dn=example,dn=com' -P 'passw0rd' -p 8636 --secure")
 parser.add_argument('-H', dest='server', help="LDAP server on which the query will be run", required=True)
 parser.add_argument('-B', dest='basedn', help="Base DN for LDAP query", required=True)
-parser.add_argument('-K', dest='key', help="Filter by a single search key (uid, cn, etc.)")
-parser.add_argument('-V', dest='value', help="Value of the search key ('john', 'sarah', 5011, etc.)")
+parser.add_argument('-K', dest='key', help="Filter by a single search key (uid, cn, etc.)", required=True)
+parser.add_argument('-V', dest='value', help="Value of the search key ('john', 'sarah', 5011, etc.)", required=True)
 parser.add_argument('-U', dest='binddn', help="Bind DN for authentication")
 parser.add_argument('-P', dest='bindpw', help="Bind password for authentication")
 parser.add_argument('-p', dest='port', help="Optional port setting")
